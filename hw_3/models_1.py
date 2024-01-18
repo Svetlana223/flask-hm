e2e8fa329ce4e968d04b40804bde0b9af456a6c4
Svetlana <strel_sv@bk.ru>
2024-01-17 23:54:31 -0500

    add hm3

---
+++ hw_3/models_1.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(40), nullable=False)
+    confirm_password = db.Column(db.String(40), nullable=False)
